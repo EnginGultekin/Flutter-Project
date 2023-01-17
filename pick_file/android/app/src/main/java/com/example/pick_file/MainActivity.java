10d061c315d07a8199f67c2d1ff90e6029229dc2
EnginGultekin <86952480+EnginGultekin@users.noreply.github.com>
2023-01-17 12:50:12 +0300

    Delete pick_file directory

--- pick_file/android/app/src/main/java/com/example/pick_file/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.pick_file;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- pick_file/lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:pick_file/main_app.dart';
-
-void main() => runApp(UploadApp());
